b5dfb9802971cbc764d269aab1a77c2b4d2c82c4
Alexander Meshcheriakov <mes-aleksandr@yandex.ru>
2020-10-03 22:26:34 +0300

    удаление папок

--- AkhmetzanovM/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Layout from './components/Layout';
-import './css/style.css';
-
-ReactDOM.render(<Layout userName="Bob" />, document.getElementById('hello-ex'));
--- Vagan_Petrosyan/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import HelloMessage from "./components/Layout";
-
-
-ReactDOM.render(<HelloMessage />, document.getElementById("root"));
