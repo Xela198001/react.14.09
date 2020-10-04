5af318e9e3f0228e9c081621bef188ce735f0410
Alexander Meshcheriakov <mes-aleksandr@yandex.ru>
2020-10-04 22:54:23 +0300

    подключение redux

--- homework5/src/Components/App/App.jsx
+++ homework5/src/Components/App/App.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
 import RootRouter from "../../Pages/RootRouter/RootRouter";
+import store from "../../store";
 
 const theme = createMuiTheme();
 
 const App = () => {
   return (
+    <Provider store={store}>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <RootRouter />
       </ThemeProvider>
     </BrowserRouter>
+    </Provider>
   );
 };
 
--- homework5/src/Pages/Chats/Chats.jsx
+++ homework5/src/Pages/Chats/Chats.jsx
@@ -25,23 +25,7 @@ const styles = (theme) => ({
 
 class Chats extends Component {
   state = {
-    chats: {
-      1: {
-        id: 1,
-        title: "Чат 1",
-        messageList: [1],
-      },
-      2: {
-        id: 2,
-        title: "Чат 2",
-        messageList: [2],
-      },
-      3: {
-        id: 3,
-        title: "Чат 3",
-        messageList: [3],
-      },
-    },
+    
     messages: {
       1: {
         id: 1,
