4d9e707dd04601f699a3fe2e446b109040db9f6a
Alexander Meshcheriakov <mes-aleksandr@yandex.ru>
2020-10-07 16:26:52 +0300

    удаление папок

--- AkhmetzanovM/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import chatReducer from '../reducers/chatReducer';
-
-export default configureStore({
-  reducer: {
-    chats: chatReducer,
-  },
-});
--- PogorelovA/src/selectors/profileSelectors.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const getFullName = state => {
-  const { firstName, lastName } = state.profile;
-  return `${firstName} ${lastName}`;
-};
